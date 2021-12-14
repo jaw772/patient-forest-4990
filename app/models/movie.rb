97be5eb9b260bf7d4f94dbd6eb6547e24713aabf
Josh Walsh <87674632+jaw772@users.noreply.github.com>
2021-12-14 15:32:54 -0700

    Test and Feat complete for story 2, 1hr22min

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -2,7 +2,7 @@ class Movie < ApplicationRecord
   belongs_to :studio
   has_many :actors
   def average_age
-    self.actors.average(:age)
+    self.actors.sum(:age).to_f / self.actors.count
   end
 
 end
--- spec/features/movies/show_spec.rb
+++ spec/features/movies/show_spec.rb
@@ -8,17 +8,21 @@ RSpec.describe 'the show page' do
     let!(:bob){raider.actors.create!(name: 'Bob', age: 27)}
     let!(:frank){raider.actors.create!(name: 'Frank', age: 54)}
     let!(:donkey){shrek.actors.create!(name: 'Donkey', age: 6)}
-    it "they see each movie's title, creation_year, and genre and a list of all actors" do
+    it "they see each movie's title, creation_year, and genre and a list of all actors ordered by age, youngest first" do
       visit "/movies/#{raider.id}"
 
       expect(page).to have_content(raider.title)
       expect(page).to have_content(raider.creation_year)
       expect(page).to have_content(raider.genre)
 
-      save_and_open_page
-      expect(page).to have_content(bob.name)
+      # save_and_open_page
+      expect(bob.name).to appear_before(frank.name)
       expect(page).to_not have_content(donkey.name)
     end
+    it "shows average age of actors" do
+      visit "/movies/#{raider.id}"
 
+      expect(page).to have_content(40.5)
+    end
   end
 end
