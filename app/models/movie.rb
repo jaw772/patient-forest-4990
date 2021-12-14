4995aec6f7e9fa9b301a518c99f7ea8a602ebb5c
Josh Walsh <87674632+jaw772@users.noreply.github.com>
2021-12-14 15:14:21 -0700

    Test and Feat: Story 2 in progress, 1hr

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,3 +1,8 @@
 class Movie < ApplicationRecord
   belongs_to :studio
+  has_many :actors
+  def average_age
+    self.actors.average(:age)
+  end
+
 end
---
+++ spec/features/movies/show_spec.rb
@@ -0,0 +1,24 @@
+require 'rails_helper'
+
+RSpec.describe 'the show page' do
+  describe 'When a user visits the movies show page' do
+    let!(:universal){Studio.create!(name: 'Universal Studios', location: 'Hollywood')}
+    let!(:raider){universal.movies.create!(title: 'Raiders of the Lost Ark', creation_year: 1981, genre: 'Action/Adventure')}
+    let!(:shrek){universal.movies.create!(title: 'Shrek', creation_year: 2001, genre: 'Comedy')}
+    let!(:bob){raider.actors.create!(name: 'Bob', age: 27)}
+    let!(:frank){raider.actors.create!(name: 'Frank', age: 54)}
+    let!(:donkey){shrek.actors.create!(name: 'Donkey', age: 6)}
+    it "they see each movie's title, creation_year, and genre and a list of all actors" do
+      visit "/movies/#{raider.id}"
+
+      expect(page).to have_content(raider.title)
+      expect(page).to have_content(raider.creation_year)
+      expect(page).to have_content(raider.genre)
+
+      save_and_open_page
+      expect(page).to have_content(bob.name)
+      expect(page).to_not have_content(donkey.name)
+    end
+
+  end
+end
