1e0f209247628eef31c52ed39de45c2795a5eb91
Josh Walsh <87674632+jaw772@users.noreply.github.com>
2021-12-14 14:41:07 -0700

    Feat: user story 1 in progress, 30min

--- spec/features/studios/index_spec.rb
+++ spec/features/studios/index_spec.rb
@@ -2,8 +2,9 @@ require 'rails_helper'
 
 RSpec.describe 'the index page' do
   describe 'When a user visits the studio index page' do
-    let!(:universal){Studio.create!(name: 'Universal Studios', location: 'Hollywood')
-}
+    let!(:universal){Studio.create!(name: 'Universal Studios', location: 'Hollywood')}
+    let!(:raider){universal.movies.create!(title: 'Raiders of the Lost Ark', creation_year: 1981, genre: 'Action/Adventure')}
+    let!(:shrek){universal.movies.create!(title: 'Shrek', creation_year: 2001, genre: 'Comedy')}
     it "they see each studios name and location" do
       visit '/studios'
 
@@ -11,9 +12,10 @@ RSpec.describe 'the index page' do
       expect(page).to have_content(universal.location)
     end
 
-    xit "underneath each studio there is all of its movies" do
-
-      expect(universal.location).to appear_after()
+    it "underneath each studio there is all of its movies" do
+      visit '/studios'
+      save_and_open_page
+      expect(universal.name).to appear_before(raider.title)
     end
   end
-end 
+end
